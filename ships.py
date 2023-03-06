0e67a62c39ea1e5e628764e1193c5ff8f534a3d9
Wadir Malik <wadir.malik@xandertalent.com>
2023-03-06 16:17:54 +0000

    completed project

---
+++ ships.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+ships = pd.read_csv(r"C:\Users\WadirMalik\Downloads\ship.csv")
+
+#print(ships.count())
+#print(ships.dtypes)
+
+#print(ships.describe())
+
+#print(ships.isnull().values.any())
+
+#print(sum(pd.isnull(ships['Age'])))
+#avg_age = ships['Age'].mean()
+#ships['Age'].fillna(avg_age, inplace=True)
+
+#ships.drop(['SibSp'], axis=1, inplace=True)
+
+#print(ships.groupby('Gender')['PassengerId'].count())
+
+ships['title'] = ships['Name'].apply(lambda x : x.split('.')[0].split(',')[1].strip())
+print(ships['title'])
+
+#half = ships['Name'].str.split(pat='.', expand=True)
+#full = half.str.split(pat=',', expand = True)
+
